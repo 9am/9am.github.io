7d2c04f73d90de7e248e44e24e2366f01e9bd529
9am <tech.9am@gmail.com>
2022-08-25 14:54:18 +0800

    fix(img-victor)

--- assets/js/img-victor/main.js
+++ assets/js/img-victor/main.js
@@ -3,10 +3,9 @@ import Pool from './pool.js';
 const createWorker = async () => {
     const slowWorker = await import('./workers/lsd/slowWorker.js?raw');
     return new Worker(
-        URL.createObjectURL(new Blob(
-            [slowWorker.default],
-            {type: 'application/script'},
-        )),
+        URL.createObjectURL(
+            new Blob([slowWorker.default], { type: 'application/script' })
+        )
     );
 };
 
@@ -58,6 +57,7 @@ template.innerHTML = `
             stroke-dashoffset: 0;
         }
         :host(:not(.active)) #path.ready {
+            transition: unset;
             stroke-dashoffset: var(--dashoffset);
         }
         #img {
@@ -121,191 +121,223 @@ template.innerHTML = `
     <image id="img" src="" part="img" crossorigin="anonymous" />
 `;
 
+const getComponent = ({ pool, e = 50e5 }) =>
+    class ImageVictor extends HTMLElement {
+        #svg;
+        #img;
+        #path;
+        #title;
+        #imgData;
+        #dList;
+        #io;
 
-const getComponent = ({
-    pool,
-    e = 50e5,
-}) => class ImageVictor extends HTMLElement {
-    #svg;
-    #img;
-    #path;
-    #title;
-    #imgData;
-    #dList;
-    #io;
-
-    static get observedAttributes() {
-        return ['src', 'title'];
-    }
-
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.#svg = this.shadowRoot.querySelector('#svg');
-        this.#img = this.shadowRoot.querySelector('#img');
-        this.#path = this.shadowRoot.querySelector('#path');
-        this.#title = this.shadowRoot.querySelector('#title');
-        this.#imgData = {};
-        this.#dList = [];
-        this.#img.addEventListener('load', this.#onImageLoaded);
-        this.#img.addEventListener('error', this.#onImageError);
-        this.#preserveRatio();
-    }
-
-    #getDataFromImage(img) {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d', { alpha: false });
-        const scale = Math.round(Math.sqrt(e / img.naturalWidth / img.naturalHeight));
-        const width = img.naturalWidth * scale;
-        const height = img.naturalHeight * scale;
-        this.ratio = `${img.naturalWidth}:${img.naturalHeight}`;
-        ctx.imageSmoothingEnabled = false;
-        canvas.width = width;
-        canvas.height = height;
-        ctx.filter = 'grayscale(1)';
-        ctx.drawImage(img, 0, 0, width, height);
-        return ctx.getImageData(0, 0, width, height);
-    }
-
-    #onImageError = (err) => {
-        this.shadowRoot.host.classList.remove('loading');
-        throw err;
-    }
-
-    #onImageLoaded = async (evt) => {
-        if (evt.target.getAttribute('src').match(/data:image\/svg\+xml/)) {
-            return;
+        static loadImage(url = '') {
+            return new Promise((resolve, reject) => {
+                let img = new Image();
+                img.crossOrigin = 'anonymous';
+                img.id = 'img';
+                img.part = 'img';
+                img.onload = () => {
+                    try {
+                        const canvas = document.createElement('canvas');
+                        const ctx = canvas.getContext('2d', { alpha: false });
+                        const scale = Math.round(Math.sqrt(e / img.width / img.height));
+                        const width = img.width * scale;
+                        const height = img.height * scale;
+                        ctx.imageSmoothingEnabled = false;
+                        canvas.width = width;
+                        canvas.height = height;
+                        ctx.filter = 'grayscale(1)';
+                        ctx.drawImage(img, 0, 0, width, height);
+                        resolve({
+                            img,
+                            imgData: ctx.getImageData(0, 0, width, height),
+                        });
+                    } catch (error) {
+                        reject(error);
+                    }
+                };
+                img.onerror = (error) => reject(error);
+                img.src = url;
+            });
         }
-        this.#imgData = this.#getDataFromImage(evt.target);
-        await this.#renderPath();
-        this.shadowRoot.host.classList.remove('loading');
-    }
 
-    #preserveRatio() {
-        const [width, height] = this.ratio?.split(':') || [];
-        if (width && height) {
-            this.#img.setAttribute('src', `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"%3E%3C/svg%3E`);
+        static get observedAttributes() {
+            return ['src', 'title'];
         }
-    }
 
-    attributeChangedCallback(name, prev, next) {
-        if (prev === next) {
-            return;
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+            this.#svg = this.shadowRoot.querySelector('#svg');
+            this.#img = this.shadowRoot.querySelector('#img');
+            this.#path = this.shadowRoot.querySelector('#path');
+            this.#title = this.shadowRoot.querySelector('#title');
+            this.#imgData = {};
+            this.#dList = [];
+            this.#preserveRatio();
         }
-        switch (name) {
-            case 'src': {
-                if (!this.src) {
+
+        async attributeChangedCallback(name, prev, next) {
+            if (prev === next) {
+                return;
+            }
+            switch (name) {
+                case 'src': {
+                    if (!this.src) {
+                        break;
+                    }
+                    try {
+                        this.shadowRoot.host.classList.add('loading');
+                        this.#preserveRatio();
+                        const { img, imgData } = await ImageVictor.loadImage(this.src);
+                        this.#img.parentNode.replaceChild(img, this.#img);
+                        this.#img = img;
+                        this.#imgData = imgData;
+                        await this.#renderPath();
+                    } finally {
+                        this.shadowRoot.host.classList.remove('loading');
+                    }
                     break;
                 }
-                this.shadowRoot.host.classList.add('loading');
-                this.#img.setAttribute('src', this.src);
-                break;
+                case 'title':
+                    this.#title.textContent = this.title;
+                    break;
+                default:
+                    break;
             }
-            case 'title':
-                this.#title.textContent = this.title;
-                break;
-            default:
-                break;
         }
-    }
 
-    async #renderPath() {
-        if (!this.#imgData.data) {
-            return;
+        #preserveRatio() {
+            const [width, height] = this.ratio?.split(':') || [];
+            if (width && height) {
+                this.#img.setAttribute(
+                    'src',
+                    `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"%3E%3C/svg%3E`
+                );
+            }
         }
-        const [lines, groups] = await pool.addTask(this.#imgData);
-        this.#dList = groups.map(
-            group => 'M' + group.map(([x1, y1, x2, y2]) => `${x1},${y1} L${x2},${y2} `).join('L'),
-        );
-        this.#path.setAttribute('d', this.#dList.join(''));
-        this.#svg.setAttribute('viewBox', `0 0 ${this.#imgData.width} ${this.#imgData.height}`);
-        this.draw();
-    }
 
-    draw() {
-        const len = Math.max.apply(null, this.#dList.map(
-            d => {
-                const ele = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-                ele.setAttribute('d', d);
-                return ele.getTotalLength();
-            },
-        ));
-        this.#path.classList.remove('ready');
-        this.#svg.style.setProperty('--dasharray', len);
-        this.#svg.style.setProperty('--dashoffset', len);
-        requestAnimationFrame(() => {
+        async #renderPath() {
+            if (!this.#imgData.data) {
+                return;
+            }
+            const [lines, groups] = await pool.addTask(this.#imgData);
+            this.#dList = groups.map(
+                (group) =>
+                    'M' +
+                    group
+                        .map(
+                            ([x1, y1, x2, y2]) =>
+                                `${x1 | 0},${y1 | 0} L${x2 | 0},${y2 | 0} `
+                        )
+                        .join('L')
+            );
+            this.#path.setAttribute('d', this.#dList.join(''));
+            this.#svg.setAttribute(
+                'viewBox',
+                `0 0 ${this.#imgData.width} ${this.#imgData.height}`
+            );
+            this.draw();
+        }
+
+        draw() {
+            const len = Math.max.apply(
+                null,
+                this.#dList.map((d) => {
+                    const ele = document.createElementNS(
+                        'http://www.w3.org/2000/svg',
+                        'path'
+                    );
+                    ele.setAttribute('d', d);
+                    return ele.getTotalLength();
+                })
+            );
+            this.#path.classList.remove('ready');
+            this.#svg.style.setProperty('--dasharray', len);
+            this.#svg.style.setProperty('--dashoffset', len);
             requestAnimationFrame(() => {
-                this.#path.classList.add('ready')
+                requestAnimationFrame(() => {
+                    this.#path.classList.add('ready');
+                });
             });
-        });
-    }
-
-    get src() {
-        return this.getAttribute('src');
-    }
-
-    set src(val = '') {
-        this.setAttribute('src', val);
-    }
-
-    get title() {
-        return this.getAttribute('title');
-    }
-
-    set title(val = '') {
-        this.setAttribute('title', val);
-    }
-
-    get ratio() {
-        return this.getAttribute('ratio');
-    }
-
-    set ratio(val = '') {
-        this.setAttribute('ratio', val);
-    }
-
-    get manual() {
-        return this.hasAttribute('manual');
-    }
-
-    set manual(val = false) {
-        if (val) {
-            this.setAttribute('maual', '');
-        } else {
-            this.removeAttribute('maual');
-        }
-    }
-
-    connectedCallback() {
-        if (!this.src) {
-            this.src = '';
         }
 
-        this.#io = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                const visible = entry.intersectionRatio > 0
-                if (visible) {
-                    entry.target.src = entry.target.dataset?.src || entry.target.src;
-                }
-                if (!this.manual) {
-                    entry.target.classList.toggle('active', visible);
-                }
-            })
-        })
-        this.#io.observe(this.shadowRoot.host);
-    }
+        get src() {
+            return this.getAttribute('src');
+        }
 
-    disconnectedCallback() {
-        this.#io.unobserve(this.shadowRoot.host);
-        this.#svg = null;
-        this.#path = null;
-        this.#title = null;
-        this.#img = null;
-        this.#imgData = null;
-        this.#dList = null;
-    }
-}
+        set src(val = '') {
+            this.setAttribute('src', val);
+        }
+
+        get title() {
+            return this.getAttribute('title');
+        }
+
+        set title(val = '') {
+            this.setAttribute('title', val);
+        }
+
+        get ratio() {
+            return this.getAttribute('ratio');
+        }
+
+        set ratio(val = '') {
+            this.setAttribute('ratio', val);
+        }
+
+        get manual() {
+            return this.hasAttribute('manual');
+        }
+
+        set manual(val = false) {
+            if (val) {
+                this.setAttribute('maual', '');
+            } else {
+                this.removeAttribute('maual');
+            }
+        }
+
+        connectedCallback() {
+            if (!this.src) {
+                this.src = '';
+            }
+
+            this.#io = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        const visible = entry.intersectionRatio > 0;
+                        if (visible) {
+                            entry.target.src =
+                                entry.target.dataset?.src || entry.target.src;
+                        }
+                        if (!this.manual) {
+                            entry.target.classList.toggle('active', visible);
+                        }
+                    });
+                },
+                {
+                    threshold: [0.0, 0.5],
+                }
+            );
+            this.#io.observe(this.shadowRoot.host);
+        }
+
+        disconnectedCallback() {
+            this.#io.unobserve(this.shadowRoot.host);
+            this.#io = null;
+            this.#svg = null;
+            this.#img.onload = null;
+            this.#img.onerror = null;
+            this.#img = null;
+            this.#path = null;
+            this.#title = null;
+            this.#imgData = null;
+            this.#dList = null;
+        }
+    };
 
 class Fallback extends HTMLElement {
     constructor() {
@@ -329,22 +361,18 @@ export const register = async ({
     try {
         const pool = new Pool({
             worker,
-            size: window.navigator.hardwareConcurrency && window.navigator.hardwareConcurrency > 1
-                ? Math.max(1, poolSize)
-                : 1,
+            size:
+                window.navigator.hardwareConcurrency &&
+                window.navigator.hardwareConcurrency > 1
+                    ? Math.max(1, poolSize)
+                    : 1,
         });
         const Component = getComponent({
             pool,
         });
-        window.customElements.define(
-            tagName,
-            Component,
-        );
+        window.customElements.define(tagName, Component);
     } catch (err) {
-        window.customElements.define(
-            tagName,
-            Fallback,
-        );
+        window.customElements.define(tagName, Fallback);
     }
 };
 
